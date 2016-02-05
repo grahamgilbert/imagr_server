1bf76b16668cd7be7cbd77d7644523bd7923359c
Victor Vrantchan <vrancean+github@gmail.com>
2016-02-04 19:01:11 -0500

    create slack notification

--- report/views.py
+++ report/views.py
@@ -1,3 +1,6 @@
+import json
+import requests
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -21,4 +24,25 @@ def index(request):
         # create a new report object
         report = Report(computer=computer, message=message, status=status)
         report.save()
+        if settings.SLACK_NOTIFY:
+            try:
+                notify_slack_channel(serial, message, status)
+            except:
+                return HttpResponse('Error sending slack notification')
         return HttpResponse(data)
+
+
+def notify_slack_channel(serial, message, status):
+    text = "%s %s %s" % (serial, status, message)
+    url = settings.SLACK_WEBHOOK_URL
+    channel = settings.SLACK_CHANNEL
+    user = settings.SLACK_BOT_NAME
+    emoji =''
+    headers = {'content-type':'application/json'}
+    payload = json.dumps({
+        'channel' : channel,
+        'username' : user,
+        'text' : text,
+        'icon_emoji' : emoji,
+        })
+    request = requests.post(url, headers=headers, data=payload)
